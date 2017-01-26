2ad63d7f90c90cdbb9cad4cccb9eff0e6db74b16
anttilip <antti.lip@gmail.com>
2017-01-26 04:49:52 +0200

    Add more comments to Dijkstra

--- src/Pathfinding-algos/Dijkstra.js
+++ src/Pathfinding-algos/Dijkstra.js
@@ -6,7 +6,8 @@ class Dijkstra {
         // TODO: implementoi oma hash set
         var alreadySeen = new Set();
 
-        var flatten = (node) => ((node.x) * graph.size + node.y);
+        // Convert node to 1D array index
+        var flatten = (node) => (node.x * graph.size + node.y);
 
         // Initialize distances
         for (var i = 0; i < dist.length; i++) {
@@ -17,29 +18,36 @@ class Dijkstra {
         dist[flatten(graph.startNode)] = 0;
         nodes.push(0, graph.startNode);
 
+        // Main loop
         while (nodes.size !== 0) {
+            // Get node with smallest distance
             var node = nodes.pop().node;
+            // Find nodes neighbours
             var neighbours = graph.getNodesNeighbours(node);
             for (var i = 0; i < neighbours.length; i++) {
                 var neighbour = neighbours[i];
                 // TODO: add different weight for diagonals, i.e. sqrt(2)
-                var currentDistance = dist[flatten(node)] + 1;
-
+                // Neighbours distance will be current nodes distance + 1
+                var neighboursDistance = dist[flatten(node)] + 1;
+                // If neighbour is already visited, this path must be longer so
+                // no need to continue checking it.
                 if (!alreadySeen.has(neighbour)) {
-                    dist[flatten(neighbour)] = currentDistance;
+                    // Save neighbours distance
+                    dist[flatten(neighbour)] = neighboursDistance;
+                    // Set neighbours "parent" node to current node
                     prev[flatten(neighbour)] = node;
                     if (graph.goalNode.equals(neighbour)) {
-                        // Goal node was found
-                        console.log('löyty')
+                        // Goal node was found, return prev to find out path
                         return prev;
                     }
+                    // Set neighbour to seen
                     alreadySeen.add(neighbour);
-                    nodes.push(currentDistance, neighbour);
+                    // Push neighbour to heap with its distance as a key
+                    nodes.push(neighboursDistance, neighbour);
                 }
             }
         }
         // Goal node was not found, i.e. path does not exist.
         return prev;
-
     }
 }
