96558c6f385be56d0ea0ca255098d90e175d4664
anttilip <antti.lip@gmail.com>
2017-02-21 18:27:01 +0200

    Add Depth first search algorithm

---
+++ src/pathfinders/dfs.js
@@ -0,0 +1,45 @@
+class DFS extends Finder {
+    constructor(...opts) {
+        super(...opts);
+    }
+
+    findShortestPath() {
+        this.stack = new List();
+        let openedList = new List();
+
+        this.grid.startNode.gScore = 0;
+        this.grid.startNode.opened = true;
+        this.stack.push(this.grid.startNode);
+
+        while (this.stack.length !== 0) {
+            let node = this.stack.pop();
+            node.closed = true;
+
+
+
+            let neighbours = this.grid.getNodesNeighbours(node);
+            for (let i = 0; i < neighbours.length; i++) {
+                let neigbour = neighbours.get(i);
+
+                let distance = this._getDistance(node, neigbour);
+                if (!neigbour.opened) {
+                    neigbour.opened = true;
+                    neigbour.parent = node;
+                    this.stack.push(neigbour);
+                    openedList.push(neigbour);
+                }
+                if (distance < neigbour.gScore) {
+                    neigbour.gScore = distance;
+                }
+
+                if (this.grid.goalNode.equals(neigbour)) {
+                    return {
+                        path: this._backtrack(neigbour),
+                        opened: openedList
+                    };
+                }
+            }
+        }
+        return {path: [], opened: openedList};
+    }
+}
---
+++ test/pathfinders/dfsTest.js
@@ -0,0 +1,21 @@
+describe('DFS', function() {
+    describe('solvable', function() {
+        solvableMazes.forEach((maze) => {
+            let grid = new Grid(undefined, matrix = maze.maze);
+            let dfs = new DFS(grid);
+            let result = dfs.findShortestPath();
+
+            it('should solve solvable maze', function() {
+                expect(grid.goalNode.gScore).to.not.be.infinity;
+            });
+        });
+    });
+
+    it('should not solve unsolvable', function() {
+        let grid = new Grid(undefined, matrix = unsolvableMaze.maze);
+        let dfs = new DFS(grid);
+        let result = dfs.findShortestPath();
+
+        expect(grid.goalNode.gScore).to.be.infinity;
+    });
+});
