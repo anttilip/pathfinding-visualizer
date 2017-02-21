9df9166c922d2108549ecc4cb1dcaf1886d2deb5
anttilip <antti.lip@gmail.com>
2017-02-21 06:48:58 +0200

    Set up config for automatic testing

---
+++ test/karma.conf.js
@@ -0,0 +1,31 @@
+module.exports = function(config) {
+  config.set({
+      frameworks: ['mocha', 'chai'],
+    files: [
+      '../src/data-structures/*.js',
+      '../src/pathfinders/*.js',
+      '../src/core/node.js',
+      '../src/core/grid.js',
+      'pathfinders/mazes.js',
+      'data-structures/*.js',
+      'core/*.js',
+      'pathfinders/*.js'
+    ],
+
+    // coverage reporter generates the coverage
+    reporters: ['progress', 'coverage'],
+
+    preprocessors: {
+      // source files, that you wanna generate coverage for
+      // do not include tests or libraries
+      // (these files will be instrumented by Istanbul)
+      '../src/**/*.js': ['coverage']
+    },
+
+    // optionally, configure the reporter
+    coverageReporter: {
+      type : 'html',
+      dir : 'coverage/'
+    }
+  });
+};
