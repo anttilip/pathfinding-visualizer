2f193b280626cc6ccf6c3738388b9cbdcce27888
Antti Lipsanen <antti.lip@gmail.com>
2017-03-10 23:57:10 +0200

    Update list.js

--- src/data-structures/list.js
+++ src/data-structures/list.js
@@ -1,3 +1,4 @@
+/** Dynamic list */
 class List {
     constructor(initialSize = 32) {
         if (initialSize !== parseInt(initialSize) || initialSize <= 0) {
@@ -11,6 +12,10 @@ class List {
         this.last = () => this.array[this.length - 1];
     }
 
+    /**
+     * Add value to List.
+     * @param {?} value - value that will be added to the list. 
+     */
     push(value) {
         if (this.length == this.array.length) {
             this._expandArray();
@@ -19,19 +24,33 @@ class List {
         this.length++;
     }
 
+    /**
+     * Return and remove last item from list.
+     * @return {?} - last item in list.
+     */
     pop() {
         this.length--;
         return this.array[this.length];
     }
 
+    
     get(i) {
         return this.array[i];
     }
 
+    /**
+     * Set elemnt in index i to new value.
+     * @param {?} i - index.
+     * @param {?} element - new value.
+     */
     set(i, element) {
         this.array[i] = element;
     }
-
+    
+    /**
+     * Fill the list with given value.
+     * @param {?} value - value that will fill the whole list.
+     */
     fill(value) {
         for (let i = 0; i < this.array.length; i++) {
             this.array[i] = value;
@@ -39,12 +58,20 @@ class List {
         this.length = this.array.length;
     }
 
+    /**
+     * Swap elements in given indexes.
+     * @param {?} a - first index.
+     * @param {?} b - second index.
+     */
     swap(a, b) {
         let tmp = this.array[a];
         this.array[a] = this.array[b];
         this.array[b] = tmp;
     }
 
+    /**
+     * Reverse the list.
+     */
     reverse() {
         let reversed = new Array(this.array.length);
         for (let i = 0; i < this.length; i++) {
